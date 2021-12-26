42cf8fb9e2412f12f795f043521580de2e406e0b
Ashish Jaiswal <79195338+HackWithAJ@users.noreply.github.com>
2021-12-26 11:35:05 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,18 +1,14 @@
-# Program make a simple calculator
+# Python
 
-# This function adds two numbers
 def add(x, y):
     return x + y
 
-# This function subtracts two numbers
 def subtract(x, y):
     return x - y
 
-# This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
-# This function divides two numbers
 def divide(x, y):
     return x / y
 
